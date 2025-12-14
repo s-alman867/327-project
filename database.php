115060c9e4f0ad6272e727fd0f5f65b07bd07ca0
Sazid Rahman Bhuiyan <105922103+SazidBhuiyan@users.noreply.github.com>
2025-12-14 17:26:44 +0600

    Add files via upload

---
+++ database.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "karukunjo_db");
+
+if ($conn->connect_error) {
+    die("Database Connection Failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+?>
