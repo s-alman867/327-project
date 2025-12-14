dfaa9a016c5ae1b014e799e29483cad54a8863af
s-alman867 <salmantonmoy6969@gmail.com>
2025-12-14 17:07:08 +0600

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "karukunjo_db");
-
-if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8");
